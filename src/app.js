7a9309b823f19d4a91777688a4c0e70658d0ed82
unknown <rafaelheitor2@outlook.com>
2022-06-12 12:05:06 -0300

    Instalando biblioteca express-session

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
 require('./database')
 
 var indexRouter = require('./routes/index');
@@ -19,6 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+	session({
+	  resave: true,
+	  saveUninitialized: true,
+	  secret: 'palavraSecreta',
+	  cookie: {maxAge: 100 * 60 * 60 * 24}
+	})
+  )
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
