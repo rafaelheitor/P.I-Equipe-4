0df7be4eeead9547cf4203685d4b1a5f5bce87fc
Rafael Heitor <rafaelheitor2@outlook.com>
2022-06-17 16:18:08 -0300

    Enviando lista de produtos pela rota /produtos

--- backend/controller/produtosController.js
+++ backend/controller/produtosController.js
@@ -1,41 +1,45 @@
-const Produto = require('../models/Produto')
-const capturarErrosAsync = require('../middleware/capturarErrosAsync')
+const Produto = require("../models/Produto")
+const capturarErrosAsync = require("../middleware/capturarErrosAsync")
 
 const produtosController = {
-    novoProduto: capturarErrosAsync( async(req, res, next) => {
-        let {nome, descricao, valor, categoria, imagem_produto, estoque } = req.body
-        
-        const produto = await Produto.create({
-            nome,
-            descricao,
-            valor,
-            categoria,
-            imagem_produto,
-            estoque
-        })
+  todosProdutos: capturarErrosAsync(async (req, res) => {
+    const listaDeProdutos = await Produto.findAll()
+    res.send(listaDeProdutos)
+  }),
 
-        res.status(200).json({
-            success: true, 
-            produto
-        })
-    }),
-    detalheProduto: async(req, res) => {
-        let { id } = req.params
-        let produto = await Produto.findByPk(id)
+  novoProduto: capturarErrosAsync(async (req, res, next) => {
+    let { nome, descricao, valor, categoria, imagem_produto, estoque } =
+      req.body
 
-        res.render('detalhe', {produto:produto})
-    },
-    deletaProduto: async(req, res) => {
-        let { id } = req.params
-        let produto = await Produto.destroy({
-            where: {
-                id
-            }
-        })
-        res.send("Produto deletado")
+    const produto = await Produto.create({
+      nome,
+      descricao,
+      valor,
+      categoria,
+      imagem_produto,
+      estoque,
+    })
 
-        
-    }
+    res.status(200).json({
+      success: true,
+      produto,
+    })
+  }),
+  detalheProduto: async (req, res) => {
+    let { id } = req.params
+    let produto = await Produto.findByPk(id)
+
+    res.render("detalhe", { produto: produto })
+  },
+  deletaProduto: async (req, res) => {
+    let { id } = req.params
+    let produto = await Produto.destroy({
+      where: {
+        id,
+      },
+    })
+    res.send("Produto deletado")
+  },
 }
 
 module.exports = produtosController
--- backend/routes/produtos.js
+++ backend/routes/produtos.js
@@ -1,11 +1,26 @@
-const express = require('express')
+const express = require("express")
 const router = express()
-const produtosController = require('../controller/produtosController')
-const validaUsuarioLogado = require('../middleware/validaUsuarioLogado')
-const verificaPrivilegios = require('../middleware/verificaPrivilegios')
+const produtosController = require("../controller/produtosController")
+const validaUsuarioLogado = require("../middleware/validaUsuarioLogado")
+const verificaPrivilegios = require("../middleware/verificaPrivilegios")
 
-router.get('/:id/detalhe', validaUsuarioLogado, produtosController.detalheProduto)
-router.post('/novo', validaUsuarioLogado, verificaPrivilegios, produtosController.novoProduto)
-router.delete('/deletar/:id', validaUsuarioLogado, verificaPrivilegios, produtosController.deletaProduto)
+router.get("/", produtosController.todosProdutos)
+router.get(
+  "/:id/detalhe",
+  validaUsuarioLogado,
+  produtosController.detalheProduto
+)
+router.post(
+  "/novo",
+  validaUsuarioLogado,
+  verificaPrivilegios,
+  produtosController.novoProduto
+)
+router.delete(
+  "/deletar/:id",
+  validaUsuarioLogado,
+  verificaPrivilegios,
+  produtosController.deletaProduto
+)
 
 module.exports = router
