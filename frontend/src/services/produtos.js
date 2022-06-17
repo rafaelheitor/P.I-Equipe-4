fad2e9ce80bec0676cebc0ee0892cfc0fc9f7f40
unknown <rafaelheitor2@outlook.com>
2022-06-16 22:25:17 -0300

    Adicionando axios e criando objeto para requisição de produtos

---
+++ frontend/src/services/produtos.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:4000/'
+
+const apiProdutos = {
+    get: (endpoit) => axios.get(baseUrl + endpoit),
+    create: (endpoit, data) => axios.post(baseUrl + endpoit, data)
+}
+
+export default apiProdutos
