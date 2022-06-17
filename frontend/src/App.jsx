f7786e3df032df78f8d76643980b5b75ef1e207a
Rafael Heitor <rafaelheitor2@outlook.com>
2022-06-17 15:15:05 -0300

    Estilizando footer, adicionando produtos.json para testar Componente CardProduto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,38 +2,23 @@ import React from "react"
 import CardProduto from "./components/CardProduto"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
+import produtos from "./produtos.json"
 import "./App.css"
 
 export default function App() {
+  const produtosCard = produtos.map((produto) => (
+    <CardProduto
+      name={produto.nome}
+      valor={produto.valor}
+      imagem={produto.imagem_produto}
+      categoria={produto.categoria}
+    />
+  ))
+
   return (
     <div>
       <Header />
-      <div className="catalogo">
-        <CardProduto
-          nome="Ração de cachorro"
-          valor="50,00"
-          categoria="Alimentação"
-          imagem="https://static.petz.com.br/fotos/1630012033940.jpg"
-        />
-        <CardProduto
-          nome="Ração de cachorro"
-          valor="50,00"
-          categoria="Alimentação"
-          imagem="https://static.petz.com.br/fotos/1630012033940.jpg"
-        />
-        <CardProduto
-          nome="Ração de cachorro"
-          valor="50,00"
-          categoria="Alimentação"
-          imagem="https://static.petz.com.br/fotos/1630012033940.jpg"
-        />
-        <CardProduto
-          nome="Ração de cachorro"
-          valor="50,00"
-          categoria="Alimentação"
-          imagem="https://static.petz.com.br/fotos/1630012033940.jpg"
-        />
-      </div>
+      <div className="catalogo">{produtosCard}</div>
       <Footer />
     </div>
   )
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,57 @@
 import React from "react"
 
 export default function Footer() {
-  return <h1>Footer</h1>
+  return (
+    <footer>
+      <div className="social">
+        <div>
+          <h4>Redes Sociais</h4>
+          <ul>
+            <li>
+              <img src="/img/instagram-brands (1).svg" alt="Instagram" />
+              <a></a>Instagram
+            </li>
+            <li>
+              <img alt="Facebook" />
+              <a></a>Facebook
+            </li>
+            <li>
+              <img src="/img/twitter-brands.svg" alt="Twitter" />
+              <a></a>Twitter
+            </li>
+          </ul>
+        </div>
+
+        <div className="enderco">
+          <h4>Endereço</h4>
+          <p>Rua das Creoulas 456 Recife - PE</p>
+          <p>Cep: 50000000</p>
+        </div>
+
+        <div className="central">
+          <h4>Central do Cliente</h4>
+          <ul>
+            <li>Pedidos</li>
+            <li>Cadastro</li>
+            <li>Ouvidoria</li>
+          </ul>
+        </div>
+
+        <div className="departamento">
+          <h4>Departamentos</h4>
+          <ul>
+            <li>
+              <img src="/img/dog-solid.svg" alt="Departamento de Cães" />
+            </li>
+            <li>
+              <img src="/img/cat-solid.svg" alt="Departamento de Gatos" />
+            </li>
+            <li>
+              <img src="/img/fish-solid.svg" alt="Departamento de Peixes" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  )
 }
