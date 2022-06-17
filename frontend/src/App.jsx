d213a4f9254f9d9616551ffc874167171c2998b0
Rafael Heitor <rafaelheitor2@outlook.com>
2022-06-17 16:20:05 -0300

    Fetching /produtos, adicionando produtos ao state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,30 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import CardProduto from "./components/CardProduto"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
-import produtos from "./produtos.json"
+import produtosApi from "./services/produtos"
 import "./App.css"
 
 export default function App() {
-  const produtosCard = produtos.map((produto) => (
+  const [apiData, setApiData] = useState([])
+
+  const fetchProdutos = async () => {
+    try {
+      const { data: produtos } = await produtosApi.get("/produtos")
+      console.log(produtos)
+      setApiData(produtos)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchProdutos()
+  }, [])
+
+  const produtosCard = apiData.map((produto) => (
     <CardProduto
-      name={produto.nome}
+      nome={produto.nome}
       valor={produto.valor}
       imagem={produto.imagem_produto}
       categoria={produto.categoria}
--- frontend/src/services/produtos.js
+++ frontend/src/services/produtos.js
@@ -1,10 +1,10 @@
-import axios from 'axios'
+import axios from "axios"
 
-const baseUrl = 'http://localhost:4000/'
+const baseUrl = "http://localhost:4000"
 
 const apiProdutos = {
-    get: (endpoit) => axios.get(baseUrl + endpoit),
-    create: (endpoit, data) => axios.post(baseUrl + endpoit, data)
+  get: (endpoit) => axios.get(baseUrl + endpoit),
+  create: (endpoit, data) => axios.post(baseUrl + endpoit, data),
 }
 
 export default apiProdutos
