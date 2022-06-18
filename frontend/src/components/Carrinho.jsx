de0d0395fd0d735f8c78b59b5874da1c65506521
unknown <rafaelheitor2@outlook.com>
2022-06-18 19:53:02 -0300

    Adicionando React Router Dom ao projeto

---
+++ frontend/src/components/Carrinho.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+export default function Carrinho() {
+  return (
+    <div>
+      <Header />
+      <h1>Aqui vai o carrinho</h1>
+      <Footer />
+    </div>
+  )
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,18 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Carrinho from './components/Carrinho'
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<App />)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/carrinho" element={<Carrinho />} />
+    </Routes>
+  </BrowserRouter>,
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
