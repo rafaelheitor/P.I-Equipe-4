2163272ad273058bf60e9aa022ef37b87f52f866
unknown <rafaelheitor2@outlook.com>
2022-06-25 20:44:29 -0300

    Recebendo o produto do backend e colocando no estado

--- frontend/src/views/Detalhe/index.jsx
+++ frontend/src/views/Detalhe/index.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { infoProduto } from '../../services/produtos'
 
 export default function Detalhe() {
+  const [produtoDetalhe, setProdutoDetalhe] = useState({})
   const { id } = useParams()
 
-  async function fetchProduto() {
-    let produto = await infoProduto(id)
-    console.log(produto)
-  }
+  useEffect(() => {
+    infoProduto(id).then((response) => setProdutoDetalhe(response))
+  }, [])
 
-  fetchProduto()
-  return <h1>Produto - {id} </h1>
+  return (
+    <>
+      <h1>Produto - {produtoDetalhe.id} </h1>
+      <h1>Produto - {produtoDetalhe.nome} </h1>
+    </>
+  )
 }
