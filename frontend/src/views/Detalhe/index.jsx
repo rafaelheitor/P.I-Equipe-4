3832e5049a5731f91a507656d09560dfa77972cd
unknown <rafaelheitor2@outlook.com>
2022-06-25 22:18:42 -0300

    Ajustes view detalhe

--- frontend/src/views/Detalhe/index.jsx
+++ frontend/src/views/Detalhe/index.jsx
@@ -1,19 +1,52 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { infoProduto } from '../../services/produtos'
+import Footer from '../../components/Footer'
+import Header from '../../components/Header'
 
 export default function Detalhe() {
-  const [produtoDetalhe, setProdutoDetalhe] = useState({})
+  const [produto, setProduto] = useState({})
   const { id } = useParams()
 
+  const fetchProduto = async () => {
+    try {
+      const produto = await infoProduto(id)
+      setProduto(produto)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   useEffect(() => {
-    infoProduto(id).then((response) => setProdutoDetalhe(response))
+    fetchProduto()
   }, [])
 
   return (
     <>
-      <h1>Produto - {produtoDetalhe.id} </h1>
-      <h1>Produto - {produtoDetalhe.nome} </h1>
+      <Header />
+      <main>
+        <section className="descricao">
+          <div>
+            <img src={produto.imagem_produto} />
+          </div>
+
+          <section className="s-descricao">
+            <div>
+              <h2>{produto.nome}</h2>
+              <p>{produto.id}</p>
+              <p>
+                Descrição:
+                {produto.descricao}
+              </p>
+            </div>
+
+            <div className="price">
+              <span>R${produto.valor},00</span>
+            </div>
+          </section>
+        </section>
+      </main>
+      <Footer />
     </>
   )
 }
