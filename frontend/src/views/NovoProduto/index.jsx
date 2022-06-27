ee6f37c78c0e9e0923657cd76211def1ffdde2cb
Rafael Heitor <rafaelheitor2@outlook.com>
2022-06-27 10:03:28 -0300

    Ajustes view NovoProduto e estilização da página

--- frontend/src/views/NovoProduto/index.jsx
+++ frontend/src/views/NovoProduto/index.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react'
-import Footer from '../../components/Footer'
-import Header from '../../components/Header'
+import React, { useState } from "react"
+import Footer from "../../components/Footer"
+import Header from "../../components/Header"
+import "./style.css"
 
 export default function NovoProduto() {
   const [formData, setFormData] = useState({
-    nome: '',
-    valor: '',
-    categoria: '',
-    imagem_produto: '',
-    estoque: '',
+    nome: "",
+    valor: "",
+    categoria: "",
+    imagem_produto: "",
+    estoque: "",
   })
 
   function handleChange(event) {
@@ -23,43 +24,47 @@ export default function NovoProduto() {
   return (
     <>
       <Header />
-      <form>
-        <label htmlFor="nome">Nome do produto</label>
-        <input
-          type="text"
-          name="nome"
-          value={formData.nome}
-          onChange={handleChange}
-        />
-        <label htmlFor="valor">valor do produto</label>
-        <input
-          type="text"
-          name="valor"
-          value={formData.valor}
-          onChange={handleChange}
-        />
-        <label htmlFor="categoria">Categoria do produto</label>
-        <input
-          type="text"
-          name="categoria"
-          value={formData.categoria}
-          onChange={handleChange}
-        />
-        <label htmlFor="imagem_produto">Url imagem do produto</label>
-        <input
-          type="text"
-          name="imagem_produto"
-          value={formData.imagem_produto}
-          onChange={handleChange}
-        />
-        <label htmlFor="estoque">Estoque do produto</label>
-        <input
-          type="text"
-          name="estoque"
-          value={formData.estoque}
-          onChange={handleChange}
-        />
-      </form>
+      <div className='container-produto'>
+        <h1>Adicionar produto</h1>
+        <form className='adiciona-produto'>
+          <label htmlFor='nome'>Nome</label>
+          <input
+            type='text'
+            name='nome'
+            value={formData.nome}
+            onChange={handleChange}
+          />
+          <label htmlFor='valor'>Valor</label>
+          <input
+            type='text'
+            name='valor'
+            value={formData.valor}
+            onChange={handleChange}
+          />
+          <label htmlFor='categoria'>Categoria</label>
+          <input
+            type='text'
+            name='categoria'
+            value={formData.categoria}
+            onChange={handleChange}
+          />
+          <label htmlFor='imagem_produto'>Url imagem</label>
+          <input
+            type='text'
+            name='imagem_produto'
+            value={formData.imagem_produto}
+            onChange={handleChange}
+          />
+          <label htmlFor='estoque'>Estoque</label>
+          <input
+            type='text'
+            name='estoque'
+            value={formData.estoque}
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+
       <Footer />
     </>
   )
