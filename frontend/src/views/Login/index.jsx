376cefcac1ffad9603c1f546f04c50169c2ce6ad
Rafael Heitor <rafaelheitor2@outlook.com>
2022-06-27 11:29:49 -0300

    Ajuste e estilização view Login

--- frontend/src/views/Login/index.jsx
+++ frontend/src/views/Login/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import { login } from "../../services/usuarios"
 import logo from "../../img/paw-solid.svg"
-import "../Home/App.css"
+import "./style.css"
 import { ToastContainer } from "react-toastify"
 
 export default function Login() {
@@ -27,42 +27,44 @@ export default function Login() {
   }
 
   return (
-    <div className="container-login">
-      <ToastContainer />
-      <div className="head-login">
-        <Link to="/">
-          <h1>Pet & Cia </h1>
-          <img src={logo} alt="logo" />
-          <h3>Acesse Sua conta</h3>
-        </Link>
+    <main>
+      <div className='container'>
+        <ToastContainer />
+        <div className='head-login'>
+          <Link to='/'>
+            <h1>Pet & Cia </h1>
+            <img src={logo} alt='logo' />
+            <h3>Acesse Sua conta</h3>
+          </Link>
+        </div>
+        <form>
+          <div className='controle-form'>
+            <label htmlFor='email'>Digite seu Email</label>
+            <input
+              type='text'
+              placeholder='Email'
+              name='email'
+              value={usuarioForm.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='controle-form'>
+            <label htmlFor='senha'>Digite sua senha</label>
+            <input
+              type='password'
+              placeholder='Senha'
+              name='senha'
+              value={usuarioForm.senha}
+              onChange={handleChange}
+            />
+          </div>
+          <button className='btn' type='submit' onClick={postUsuario}>
+            Entrar
+          </button>
+        </form>
+        <p>Ainda não tem registro?</p>
+        <Link to='/registro'>Registre-se</Link>
       </div>
-      <form>
-        <div className="controle-form">
-          <label htmlFor="email">Digite seu Email</label>
-          <input
-            type="text"
-            placeholder="Email"
-            name="email"
-            value={usuarioForm.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="controle-form">
-          <label htmlFor="senha">Digite sua senha</label>
-          <input
-            type="password"
-            placeholder="Senha"
-            name="senha"
-            value={usuarioForm.senha}
-            onChange={handleChange}
-          />
-        </div>
-        <button className="btn" type="submit" onClick={postUsuario}>
-          Entrar
-        </button>
-      </form>
-      <p>Ainda não tem registro?</p>
-      <Link to="/registro">Registre-se</Link>
-    </div>
+    </main>
   )
 }
