9417d0807e87e53bb64552a6105641ae96df7244
Rafael Heitor <rafaelheitor2@outlook.com>
2022-06-27 14:56:34 -0300

    Adicionando estilo pagina novo produto

--- frontend/src/views/NovoProduto/index.jsx
+++ frontend/src/views/NovoProduto/index.jsx
@@ -24,47 +24,48 @@ export default function NovoProduto() {
   return (
     <>
       <Header />
-      <div className='container-produto'>
-        <h1>Adicionar produto</h1>
-        <form className='adiciona-produto'>
-          <label htmlFor='nome'>Nome</label>
-          <input
-            type='text'
-            name='nome'
-            value={formData.nome}
-            onChange={handleChange}
-          />
-          <label htmlFor='valor'>Valor</label>
-          <input
-            type='text'
-            name='valor'
-            value={formData.valor}
-            onChange={handleChange}
-          />
-          <label htmlFor='categoria'>Categoria</label>
-          <input
-            type='text'
-            name='categoria'
-            value={formData.categoria}
-            onChange={handleChange}
-          />
-          <label htmlFor='imagem_produto'>Url imagem</label>
-          <input
-            type='text'
-            name='imagem_produto'
-            value={formData.imagem_produto}
-            onChange={handleChange}
-          />
-          <label htmlFor='estoque'>Estoque</label>
-          <input
-            type='text'
-            name='estoque'
-            value={formData.estoque}
-            onChange={handleChange}
-          />
-        </form>
-      </div>
-
+      <main className='novo-produto'>
+        <div className='container-produto'>
+          <h1>Adicionar produto</h1>
+          <form className='adiciona-produto'>
+            <label htmlFor='nome'>Nome</label>
+            <input
+              type='text'
+              name='nome'
+              value={formData.nome}
+              onChange={handleChange}
+            />
+            <label htmlFor='valor'>Valor</label>
+            <input
+              type='text'
+              name='valor'
+              value={formData.valor}
+              onChange={handleChange}
+            />
+            <label htmlFor='categoria'>Categoria</label>
+            <input
+              type='text'
+              name='categoria'
+              value={formData.categoria}
+              onChange={handleChange}
+            />
+            <label htmlFor='imagem_produto'>Url imagem</label>
+            <input
+              type='text'
+              name='imagem_produto'
+              value={formData.imagem_produto}
+              onChange={handleChange}
+            />
+            <label htmlFor='estoque'>Estoque</label>
+            <input
+              type='text'
+              name='estoque'
+              value={formData.estoque}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+      </main>
       <Footer />
     </>
   )
