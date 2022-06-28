9c6861d8526718d4797561e2027c8139b064c4db
Rafael Heitor <rafaelheitor2@outlook.com>
2022-06-28 14:09:00 -0300

    Ajustes css pagina Registro

--- frontend/src/views/Registro/index.jsx
+++ frontend/src/views/Registro/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import { registro } from "../../services/usuarios"
 import logo from "../../img/paw-solid.svg"
-import "../Home/App.css"
+import "./style.css"
 
 export default function Registro() {
   const [usuarioForm, setUsuarioForm] = useState({
@@ -31,50 +31,53 @@ export default function Registro() {
   }
 
   return (
-    <div className="container-login">
-      <div className="head-login">
-        <Link to="/">
-          <h1>Pet & Cia </h1>
-          <img src={logo} alt="logo" />
-          <h3>Acesse Sua conta</h3>
-        </Link>
+    <main className='registro'>
+      <div className='registro-container'>
+        <div className='head-login'>
+          <Link to='/'>
+            <h1>Pet & Cia </h1>
+            <img src={logo} alt='logo' />
+            <h3>Acesse Sua conta</h3>
+          </Link>
+        </div>
+        <form>
+          <label htmlFor='nome'>Digite seu Nome</label>
+          <div className='controle-form'>
+            <input
+              type='text'
+              placeholder='Nome'
+              name='nome'
+              value={usuarioForm.nome}
+              onChange={handleChange}
+            />
+          </div>
+          <label htmlFor='email'>Digite seu Email</label>
+          <div className='controle-form'>
+            <input
+              type='text'
+              placeholder='Email'
+              name='email'
+              value={usuarioForm.email}
+              onChange={handleChange}
+            />
+          </div>
+          <label htmlFor='senha'>Digite sua senha</label>
+          <div className='controle-form'>
+            <input
+              type='password'
+              placeholder='Senha'
+              name='senha'
+              value={usuarioForm.senha}
+              onChange={handleChange}
+            />
+          </div>
+          <button className='btn' type='submit' onClick={postUsuario}>
+            Entrar
+          </button>
+        </form>
+        <p>Já é cadastrado? </p>
+        <Link to='/login'>Login</Link>
       </div>
-      <form>
-        <div className="controle-form">
-          <label htmlFor="nome">Digite seu Nome</label>
-          <input
-            type="text"
-            placeholder="Nome"
-            name="nome"
-            value={usuarioForm.nome}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="controle-form">
-          <input
-            type="text"
-            placeholder="Email"
-            name="email"
-            value={usuarioForm.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="controle-form">
-          <label htmlFor="senha">Digite sua senha</label>
-          <input
-            type="password"
-            placeholder="Senha"
-            name="senha"
-            value={usuarioForm.senha}
-            onChange={handleChange}
-          />
-        </div>
-        <button className="btn" type="submit" onClick={postUsuario}>
-          Entrar
-        </button>
-      </form>
-      <p>Já é cadastrado? </p>
-      <Link to="/login">Login</Link>
-    </div>
+    </main>
   )
 }
